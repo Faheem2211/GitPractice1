4a7fda020084919e04451b616464c66099dbbf18
Faheem2211 <124262584+Faheem2211@users.noreply.github.com>
2023-02-13 12:52:49 +0530

    Update Samplecode.java

--- src/GitPrac/Samplecode.java
+++ src/GitPrac/Samplecode.java
@@ -5,7 +5,7 @@ public class Samplecode {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a = 60;
-		int c =100;
+		int c =10;
 		
 		System.out.println(a*c);
 	}
