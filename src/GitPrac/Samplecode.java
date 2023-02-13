270e248e6fc3c706d8e5bc5147499c2542e37567
Mohammed Faheem <mohammedf@maveric-systems.com>
2023-02-13 12:44:37 +0530

    First commit

---
+++ src/GitPrac/Samplecode.java
@@ -0,0 +1,13 @@
+package GitPrac;
+
+public class Samplecode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 60;
+		int c =100;
+		
+		System.out.println(a*c);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author mohammedf
+ *
+ */
+module GitPractice1 {
+}
